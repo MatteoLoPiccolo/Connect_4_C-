777ebf84dce90afdaaa3dfc7b4e23ea8453f6dca
MatteoLoPiccolo <lostsoul81x@gmail.com>
2024-03-19 15:14:10 +0100

    Divided the code into small methods

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "ConsoleApplication1.h"
 
 const int rows = 6;
 const int columns = 7;
@@ -16,29 +17,80 @@ static void printBoard(const char board[rows][columns]) {
     std::cout << std::endl;
 }
 
-// Function to control the win condition
-static bool checkWin(const char board[rows][columns], char player) {
-    // Check for horizontal
+// Function to initialize game board
+void InitializeTheBoard(char  board[6][7])
+{
     for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j <= columns - 4; ++j) {
-            if (board[i][j] == player && board[i][j + 1] == player &&
-                board[i][j + 2] == player && board[i][j + 3] == player) {
-                return true;
-            }
+        for (int j = 0; j < columns; ++j) {
+            board[i][j] = '-';
         }
     }
+}
+
+// Function to take input from Player
+void TakeInputsFromPlayer(int currentPlayer, char  board[6][7], int& retFlag)
+{
+    retFlag = 1;
+    std::cout << "Player " << currentPlayer << "'s turn. Enter number of column (from 1 to 7): ";
+    int column{};
+    std::cin >> column;
+    column--;
+
+    // Check if the column is valid and not full
+    if (column < 0 || column >= columns || board[0][column] != '-') {
+        std::cout << "Invalid move! Try again." << std::endl;
+        { retFlag = 3; return; };
+    }
+
+    // Find the lowest empty row in the selected column
+    int row{};
+    for (row = rows - 1; row >= 0; --row) {
+        if (board[row][column] == '-') {
+            board[row][column] = (currentPlayer == 1) ? 'R' : 'B';
+            break;
+        }
+    }
+}
+
+// Function to control win condition
+static bool checkWinCondition(const char board[rows][columns], char player) {
+    // Check for horizontal
+    bool retFlag;
+    bool retVal = CheckHorizontally(board, player, retFlag);
+    if (retFlag) return retVal;
 
     // Check for vertical
-    for (int j = 0; j < columns; ++j) {
-        for (int i = 0; i <= rows - 4; ++i) {
-            if (board[i][j] == player && board[i + 1][j] == player &&
-                board[i + 2][j] == player && board[i + 3][j] == player) {
-                return true;
-            }
-        }
-    }
+    bool retFlag;
+    bool retVal = CheckVertically(board, player, retFlag);
+    if (retFlag) return retVal;
 
     // Check for diagonal
+    bool retFlag;
+    bool retVal = CheckDiagonally(board, player, retFlag);
+    if (retFlag) return retVal;
+
+    return false;
+}
+
+// Function to control draw condition
+void CheckDrawCondition(char  board[6][7], bool& draw)
+{
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < columns; ++j) {
+            if (board[i][j] == '-') {
+                draw = false;
+                break;
+            }
+        }
+
+        if (!draw) break;
+    }
+}
+
+// Function to control 4 in a row diagonally
+bool CheckDiagonally(const char  board[6][7], char player, bool& retFlag)
+{
+    retFlag = true;
     for (int i = 0; i <= rows - 4; ++i) {
         for (int j = 0; j <= columns - 4; ++j) {
             if (board[i][j] == player && board[i + 1][j + 1] == player &&
@@ -51,70 +103,87 @@ static bool checkWin(const char board[rows][columns], char player) {
             }
         }
     }
+    retFlag = false;
+    return {};
+}
 
-    return false;
+// Function to control 4 in a row vertically
+bool CheckVertically(const char  board[6][7], char player, bool& retFlag)
+{
+    retFlag = true;
+    for (int j = 0; j < columns; ++j) {
+        for (int i = 0; i <= rows - 4; ++i) {
+            if (board[i][j] == player && board[i + 1][j] == player &&
+                board[i + 2][j] == player && board[i + 3][j] == player) {
+                return true;
+            }
+        }
+    }
+    retFlag = false;
+    return {};
+}
+
+// Function to control 4 in a row horizontally
+bool CheckHorizontally(const char  board[6][7], char player, bool& retFlag)
+{
+    retFlag = true;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j <= columns - 4; ++j) {
+            if (board[i][j] == player && board[i][j + 1] == player &&
+                board[i][j + 2] == player && board[i][j + 3] == player) {
+                return true;
+            }
+        }
+    }
+    retFlag = false;
+    return {};
+}
+
+// Function to print win messagge
+void WinMessage(int currentPlayer, bool& gameOver)
+{
+    std::cout << "Player " << currentPlayer << " wins!" << std::endl;
+    std::cout << "Thanks for playing!" << std::endl;
+    gameOver = true;
+}
+
+// Function to print draw messagge
+void DrawMessagge(bool& gameOver)
+{
+    std::cout << "It's a draw!" << std::endl;
+    gameOver = true;
 }
 
 int main() {
     char board[rows][columns]{};
 
     // Initialize the board with '-'
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < columns; ++j) {
-            board[i][j] = '-';
-        }
-    }
+    InitializeTheBoard(board);
 
     int currentPlayer = 1;
     bool gameOver = false;
 
     while (!gameOver) {
         printBoard(board);
-        std::cout << "Player " << currentPlayer << "'s turn. Enter number of column (from 1 to 7): ";
-        int column{};
-        std::cin >> column;
-        column--;
 
-        // Check if the column is valid and not full
-        if (column < 0 || column >= columns || board[0][column] != '-') {
-            std::cout << "Invalid move! Try again." << std::endl;
+        int retFlag{};
+        TakeInputsFromPlayer(currentPlayer, board, retFlag);
+        if (retFlag == 3) 
             continue;
-        }
-
-        // Find the lowest empty row in the selected column
-        int row{};
-        for (row = rows - 1; row >= 0; --row) {
-            if (board[row][column] == '-') {
-                board[row][column] = (currentPlayer == 1) ? 'R' : 'B';
-                break;
-            }
-        }
 
         // Check for a win
-        if (checkWin(board, (currentPlayer == 1) ? 'R' : 'B')) {
+        if (checkWinCondition(board, (currentPlayer == 1) ? 'R' : 'B')) {
             printBoard(board);
-            std::cout << "Player " << currentPlayer << " wins!" << std::endl;
-            std::cout << "Thanks for playing!" << std::endl;
-            gameOver = true;
+            WinMessage(currentPlayer, gameOver);
         }
         else {
             // Check for a draw
             bool draw = true;
-            for (int i = 0; i < rows; ++i) {
-                for (int j = 0; j < columns; ++j) {
-                    if (board[i][j] == '-') {
-                        draw = false;
-                        break;
-                    }
-                }
-
-                if (!draw) break;
-            }
+            CheckDrawCondition(board, draw);
 
             if (draw) {
                 printBoard(board);
-                std::cout << "It's a draw!" << std::endl;
-                gameOver = true;
+                DrawMessagge(gameOver);
             }
             else {
                 // Switch player
--- ConsoleApplication1.h
+++ ConsoleApplication1.h
@@ -5,3 +5,19 @@ void InitializeTheSizeOfArray(int& sizeOfArray);
 void InitializeTheElementsOfArray(int& input, std::unique_ptr<int[]>& array, int& index, int& counter, int sizeOfArray);
 
 void FindPeakElements(std::unique_ptr<int[]>& array, size_t i);
+
+static bool CheckHorizontally(const char  board[6][7], char player, bool& retFlag);
+
+void WinMessage(int currentPlayer, bool& gameOver);
+
+void DrawMessagge(bool& gameOver);
+
+void CheckDrawCondition(char  board[6][7], bool& draw);
+
+void TakeInputsFromPlayer(int currentPlayer, char  board[6][7], int& retFlag);
+
+void InitializeTheBoard(char  board[6][7]);
+
+static bool CheckVertically(const char  board[6][7], char player, bool& retFlag);
+
+static bool CheckDiagonally(const char  board[6][7], char player, bool& retFlag);
